3dbfe33337d54d45bf6976ff386229b4e1b5c3d8
Mario Manuel Mabande <mario99manuel@gmail.com>
2021-03-25 10:47:28 +0200

    add language support

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\UserCreateRequest;
+use App\Models\Photo;
+
+use function PHPUnit\Framework\isNull;
 
 class UserController extends Controller
 {
@@ -14,7 +18,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $users = User::all();
+        return view('admin.users.index',['users' => $users]);
     }
 
     /**
@@ -24,7 +29,7 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.users.create');
     }
 
     /**
@@ -33,9 +38,39 @@ class UserController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UserCreateRequest $request)
     {
-        //
+        return $request->all();
+        $data = $request->all();
+        $data['password'] = bcrypt($request->password);   
+        $data['photo_id'] = $this->uploadPhotoAndReturnPhotoId($request);
+
+        User::create($data);
+        return redirect('admin/users')->with('success','Utilizador adicionado.');
+    }
+
+    private function uploadPhotoAndReturnPhotoId(Request $request, User $user = null){
+        $photo_id = $user->photo_id;
+        if ($file = $request->file('photo_id')) {
+            $name = time().$file->getClientOriginalName();
+            $uploaded = $file->move('uploads/images',$name);
+            if ($uploaded) 
+                if (isNull()) {
+                    $photo = Photo::findOrFail($user->photo_id);
+                    $this->deleteOldPhotoFromLibrary($photo->file);
+                    $photo->file = $name;
+                    $photo->save();
+                }else{
+                    $photo = Photo::create(['file'=>$name]);       
+                }
+                $photo_id = $photo->id;
+        }
+        return $photo_id;
+    }
+
+    private function deleteOldPhotoFromLibrary($file){
+        if(file_exists(substr($file,1)))
+          unlink(substr($file,1));
     }
 
     /**
